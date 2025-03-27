f466c82d6d66b3811255436271ab35a87196638e
nyaomaru <nyaonyao0725@gmail.com>
2025-03-27 17:40:41 +0900

    fix: card color

--- app/ui/components/card.tsx
+++ app/ui/components/card.tsx
@@ -4,7 +4,7 @@ export function Card({ id, children }: { id?: string; children: ReactNode }) {
   return (
     <div
       id={id}
-      className='block max-w-sm p-6 bg-white border border-gray-300 rounded-lg shadow-sm dark:bg-zinc-900 dark:border-zinc-800'
+      className='block max-w-sm p-6 border rounded-lg shadow-sm bg-zinc-900 border-zinc-800'
     >
       {children}
     </div>
